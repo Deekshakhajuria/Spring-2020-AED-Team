5141163e4f2d265df7698483658e0e7d595662b9
Deeksha Khajuria <khajuria.d@husky.neu.edu>
2020-02-23 23:27:16 -0500

    Manage Travel Agency Jpanel added

---
+++ Falcon_Travel_Agency/src/Business/AirlinerDirectory.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author niramaykelkar
+ */
+public class AirlinerDirectory {
+
+    private ArrayList<Airliner> airLinerList;
+    public ArrayList<Airliner> getAirLinerList() {
+        return airLinerList;
+    }
+    
+}
