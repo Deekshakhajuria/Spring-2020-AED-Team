491ed9cfa3d7b7c9a0ee87ab79d094bbf7b8b8ec
mourya-aed <dasarapalli.m@husky.neu.edu>
2020-04-20 10:06:48 -0400

    enterprise and organizations logic

---
+++ Child Care System/Child_Care_System/db4odemo/src/Business/Role/ManagerRole.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.ManagerOrganization;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.ManagerRole.ManagerWorkAreaJPanel;
+
+/**
+ *
+ * @author niramaykelkar
+ */
+public class ManagerRole extends Role{
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new ManagerWorkAreaJPanel(userProcessContainer, account, (ManagerOrganization)organization, enterprise, business);
+    }
+}
